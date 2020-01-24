7c90466e47ef6ab0f7d00ec5f29ebad26020c279
Stephen Griffin <sgriffin@microsoft.com>
2020-01-24 14:07:13 -0500

    Sync with mfcmapi source

--- example/ExampleMapiConsoleApp.cpp
+++ example/ExampleMapiConsoleApp.cpp
@@ -8,54 +8,52 @@
 #include <iostream>
 #include <iomanip>
 
-#pragma warning( disable : 4127)
+#pragma warning(disable : 4127)
 
 #define CORg(_hr) \
-	do { \
+	do \
+	{ \
 		hr = _hr; \
 		if (FAILED(hr)) \
 		{ \
 			std::wcout << L"FAILED! hr = " << std::hex << hr << L".  LINE = " << std::dec << __LINE__ << std::endl; \
-			std::wcout << L" >>> " << (wchar_t*) L#_hr <<  std::endl; \
+			std::wcout << L" >>> " << (wchar_t*) L#_hr << std::endl; \
 			goto Error; \
 		} \
 	} while (0)
 
-int ListStoresTable(IMAPISession *pSession)
+int ListStoresTable(IMAPISession* pSession)
 {
 	HRESULT hr = S_OK;
 	CComPtr<IMAPITable> spTable;
-	SRowSet *pmrows = NULL;
+	SRowSet* pmrows = NULL;
 
 	enum MAPIColumns
 	{
 		COL_ENTRYID = 0,
 		COL_DISPLAYNAME_W,
 		COL_DISPLAYNAME_A,
-		cCols				// End marker
+		cCols // End marker
 	};
 
-	static const SizedSPropTagArray(cCols, mcols) = 
-	{	
-		cCols,
-		{
-			PR_ENTRYID,
-				PR_DISPLAY_NAME_W,
-				PR_DISPLAY_NAME_A,
-		}
-	};
+	static const SizedSPropTagArray(cCols, mcols) = {cCols,
+													 {
+														 PR_ENTRYID,
+														 PR_DISPLAY_NAME_W,
+														 PR_DISPLAY_NAME_A,
+													 }};
 
 	CORg(pSession->GetMsgStoresTable(0, &spTable));
 
-	CORg(spTable->SetColumns((LPSPropTagArray)&mcols, TBL_BATCH));
+	CORg(spTable->SetColumns((LPSPropTagArray) &mcols, TBL_BATCH));
 	CORg(spTable->SeekRow(BOOKMARK_BEGINNING, 0, 0));
 
 	CORg(spTable->QueryRows(50, 0, &pmrows));
 
 	std::wcout << L"Found " << pmrows->cRows << L" stores in MAPI profile:" << std::endl;
-	for (UINT i=0; i != pmrows->cRows; i++)
+	for (UINT i = 0; i != pmrows->cRows; i++)
 	{
-		SRow *prow = pmrows->aRow + i;
+		SRow* prow = pmrows->aRow + i;
 		LPCWSTR pwz = NULL;
 		LPCSTR pwzA = NULL;
 		if (PR_DISPLAY_NAME_W == prow->lpProps[COL_DISPLAYNAME_W].ulPropTag)
@@ -81,7 +79,7 @@ void TestSimpleMapi()
 	recip.ulRecipClass = MAPI_TO;
 	recip.lpszName = "John Doe";
 	recip.lpszAddress = "johndoe@example.com";
-	
+
 	msg.lpszSubject = "Hello World";
 	msg.lpszNoteText = "Test Body";
 	msg.lpRecips = &recip;
@@ -96,15 +94,15 @@ int __cdecl main()
 {
 	HRESULT hr = S_OK;
 
-	MAPIINIT_0  MAPIINIT= { 0, MAPI_MULTITHREAD_NOTIFICATIONS};
+	MAPIINIT_0 MAPIINIT = {0, MAPI_MULTITHREAD_NOTIFICATIONS};
 
 	//ForceOutlookMAPI(true);
 
 	//TestSimpleMapi();
 
-	CORg(MAPIInitialize (&MAPIINIT));
+	CORg(MAPIInitialize(&MAPIINIT));
 
-	{	// IMAPISession Smart Pointer Context
+	{ // IMAPISession Smart Pointer Context
 		CComPtr<IMAPISession> spSession;
 		CORg(MAPILogonEx(NULL, NULL, NULL, MAPI_UNICODE | MAPI_LOGON_UI, &spSession));
 
